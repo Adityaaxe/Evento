84bcb2d939807794cd52588f526a70157b713816
Adityaaxe <aditya.singh2021@sait.ac.in>
2025-03-29 09:15:42 +0530

    commit

--- frontend/src/Pages/EventDetailPage.jsx
+++ frontend/src/Pages/EventDetailPage.jsx
@@ -115,7 +115,7 @@ const EventDetailPage = () => {
         <div className="relative h-64">
           {event?.poster && (
             <img
-              src={`${event.poster}`}
+              src={`${event.poster}`.replace("/event/", "/")}
               alt={event.title}
               className="w-full h-full object-cover"
             />
