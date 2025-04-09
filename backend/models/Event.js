bf6af74403cd13b6cb56f7ac95a67453c540753c
Adityaaxe <aditya.singh2021@sait.ac.in>
2025-04-09 23:47:20 +0530

    commit

--- backend/models/Event.js
+++ backend/models/Event.js
@@ -6,7 +6,7 @@ const eventSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   time: { type: String, required: true }, // e.g., "3:00 PM - 5:00 PM"
   location: { type: String, required: true },
-  organizerID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Reference to the organizer
+  organizerID: { type: String , ref: "User", required: true }, // Reference to the organizer
   participants: [{ type: String , ref: "User" }], // Array of user IDs who registered
   poster: { type: String }, // Path to the poster image
   registrationDeadline: { type: Date, required: true }, // Deadline for event registration
